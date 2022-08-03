e39ed46a29ffb428e4a9ef37bd839b676bfd85ac
liandra-giacomine <liandra.giacomine@opencastsoftware.com>
2022-08-03 15:07:42 +0100

    Added generator for CC0014C

--- app/models/MessageType.scala
+++ app/models/MessageType.scala
@@ -33,18 +33,20 @@ object MessageType {
   case object CC007A extends MessageType("CC007A", "cc007a.njk", Phase4Generators.cc007aGen)
   case object CC007C extends MessageType("CC007C", "cc007c.njk", Phase5Generators.cc007cGen)
 
+  case object CC009C extends MessageType("CC009C", "cc009c.njk", Phase5Generators.cc009cGen)
+
+  case object CC013C extends MessageType("CC013C", "CC013C.njk", Phase5Generators.cc013cGen)
+
+  case object CC014C extends MessageType("CC014C", "cc014c.njk", Phase5Generators.cc014cGen)
+
   case object CC015B extends MessageType("CC015B", "cc015b.njk", Phase4Generators.cc015bGen)
   case object CC015C extends MessageType("CC015C", "cc015c.njk", Phase5Generators.cc015cGen)
-
-  case object CC044A extends MessageType("CC044A", "cc044a.njk", Phase4Generators.cc044aGen)
-  case object CC044C extends MessageType("CC044C", "cc044c.njk", Phase5Generators.cc044cGen)
 
   case object CD034A extends MessageType("CD034A", "cd034a.njk", Phase4Generators.cd034aGen)
   case object CD037A extends MessageType("CD037A", "cd037a.njk", Phase4Generators.cd037aGen)
 
-  case object CC013C extends MessageType("CC013C", "CC013C.njk", Phase5Generators.cc013cGen)
-
-  case object CC009C extends MessageType("CC009C", "cc009c.njk", Phase5Generators.cc009cGen)
+  case object CC044A extends MessageType("CC044A", "cc044a.njk", Phase4Generators.cc044aGen)
+  case object CC044C extends MessageType("CC044C", "cc044c.njk", Phase5Generators.cc044cGen)
 
   val values: Set[MessageType] = ListSet(
     CC007A,
@@ -56,7 +58,8 @@ object MessageType {
     CD034A,
     CD037A,
     CC013C,
-    CC009C
+    CC009C,
+    CC014C
   )
 
   implicit val messageTypePathBindable: PathBindable[MessageType] = new PathBindable.Parsing(
